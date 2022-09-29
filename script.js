ae978025a19519171db90ec76990186cd72e9168
Nedramrez <zermarden@gmail.com>
2022-09-29 14:18:59 +0600

    Fix linters' errors

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ class Display {
     const bookList = document.querySelector('#books-list');
 
     const tableRow = document.createElement('tr');
-    const dataCell = document.createElement('td')
+    const dataCell = document.createElement('td');
     const dataCell2 = document.createElement('td');
-    const button = document.createElement('button')
+    const button = document.createElement('button');
     dataCell.innerHTML = ` <span>"${book.title}"</span> by <span>${book.author}</span> `;
     tableRow.append(dataCell);
-    tableRow.append(dataCell2)
+    tableRow.append(dataCell2);
     dataCell2.append(button);
     button.classList.add('delete');
     button.innerText = 'Remove';
