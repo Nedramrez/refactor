5f4cc5f6d446c7e37b8afba0504a654f1f0d78b3
Nedramrez <zermarden@gmail.com>
2022-09-29 14:17:36 +0600

    Add stylings for app in a style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* eslint-disable class */
+/* eslint-disable max-classes-per-file */
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -45,17 +45,19 @@ class Display {
   }
 
   static printCollection(book) {
-    const list = document.querySelector('#books-list');
+    const bookList = document.querySelector('#books-list');
 
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-      <td id="col1">"${book.title}" by</td>
-      <td id="col2">${book.author}</td>
-      <td id="col3"><a href="#" class="delete">Remove</a></td>
-    `;
-
-    list.appendChild(row);
+    const tableRow = document.createElement('tr');
+    const dataCell = document.createElement('td')
+    const dataCell2 = document.createElement('td');
+    const button = document.createElement('button')
+    dataCell.innerHTML = ` <span>"${book.title}"</span> by <span>${book.author}</span> `;
+    tableRow.append(dataCell);
+    tableRow.append(dataCell2)
+    dataCell2.append(button);
+    button.classList.add('delete');
+    button.innerText = 'Remove';
+    bookList.appendChild(tableRow);
   }
 
   static deleteBook(el) {
@@ -93,5 +95,5 @@ document.querySelector('#form').addEventListener('submit', (e) => {
 document.querySelector('#books-list').addEventListener('click', (e) => {
   Display.deleteBook(e.target);
 
-  Storage.delBook(e.target.parentElement.previousElementSibling.textContent);
+  Storage.delBook(e.target.parentElement.previousElementSibling.lastElementChild.textContent);
 });
